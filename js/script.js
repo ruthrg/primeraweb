55ffa7fa6b0eb033e3e85a49cd23f1d9d9454818
mauricio castillo leon <ruthriang@gmail.com>
2021-04-14 22:00:25 -0500

    actualizacion 14 abr

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function validardatos() {
+    /* Comentario */
+
+    //alert("Hola mundo")
+    //prompt("Ingrese su nombre")
+    //console.log("hola mundo")
+    //document.write("HOLA MUNDO")
+
+    let nombre = document.getElementById('name').value
+
+    if(nombre=="Cristhian"){
+        alert('Bienvenido')
+    }else{
+        alert('No eres bienvenido');
+    }
+
+
+    
+
+}
+
+
